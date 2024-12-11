b1892a362b9410ff63152fe4d10b3ab2be9f061c
Ilia Mikhailov <eternalway256@gmail.com>
2024-12-11 13:24:44 +0200

    Custom swagger

--- src/main/resources/static/custom-swagger/swagger-initializer.js
+++ src/main/resources/static/custom-swagger/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:8080/custom-swagger-json",
+    url: "/custom-swagger-json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
